2125a717afbc92d55e549118560f2e2236cb6a79
Nguyễn Khánh Quy <120696079+nguyenkhanhquy@users.noreply.github.com>
2024-04-30 18:01:52 +0700

    add: Employee dao

---
+++ src/main/java/com/bookstore/api/dao/EmployeeRepository.java
@@ -0,0 +1,10 @@
+package com.bookstore.api.dao;
+
+import com.bookstore.api.entity.Employee;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface EmployeeRepository extends JpaRepository<Employee, Integer> {
+
+    // that's it ... no need to write any code
+    // findAll(), findById(), save(), deleteById(), ...
+}
