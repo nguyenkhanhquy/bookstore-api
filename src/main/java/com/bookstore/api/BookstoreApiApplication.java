147d8e84d62bc548072d3948663768a142ce3886
Nguyễn Khánh Quy <120696079+nguyenkhanhquy@users.noreply.github.com>
2024-04-30 17:50:18 +0700

    init: bookstore-api

---
+++ src/main/java/com/bookstore/api/BookstoreApiApplication.java
@@ -0,0 +1,13 @@
+package com.bookstore.api;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BookstoreApiApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(BookstoreApiApplication.class, args);
+    }
+
+}
