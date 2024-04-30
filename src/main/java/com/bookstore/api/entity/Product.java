03c63c1afd2d797bc247231b70284ef948c55767
Nguyễn Khánh Quy <120696079+nguyenkhanhquy@users.noreply.github.com>
2024-04-30 21:17:38 +0700

    add: Product entity

---
+++ src/main/java/com/bookstore/api/entity/Product.java
@@ -0,0 +1,31 @@
+package com.bookstore.api.entity;
+
+import jakarta.persistence.*;
+import lombok.*;
+
+@Entity
+@Table(name = "product")
+@NoArgsConstructor
+@AllArgsConstructor
+@Getter
+@Setter
+@ToString
+public class Product {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "id")
+    private int id;
+
+    @Column(name = "name")
+    private String name;
+
+    @Column(name = "images")
+    private String images;
+
+    @Column(name = "description", length = 1000)
+    private String description;
+
+    @Column(name = "price")
+    private double price;
+}
