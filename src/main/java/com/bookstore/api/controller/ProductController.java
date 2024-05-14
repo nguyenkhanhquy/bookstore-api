1fe57f098f20a5d31306003a821ac19760607e7f
Nguyễn Khánh Quy <120696079+nguyenkhanhquy@users.noreply.github.com>
2024-05-15 00:11:26 +0700

    feat: add product

--- src/main/java/com/bookstore/api/controller/ProductController.java
+++ src/main/java/com/bookstore/api/controller/ProductController.java
@@ -1,13 +1,17 @@
 package com.bookstore.api.controller;
 
+import com.bookstore.api.entity.user.User;
 import com.bookstore.api.response.ProductResponse;
 import com.bookstore.api.entity.product.Product;
 import com.bookstore.api.service.ProductService;
+import com.bookstore.api.util.S3Util;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
+import org.springframework.web.multipart.MultipartFile;
 
+import java.io.IOException;
 import java.util.List;
 
 @RestController
@@ -76,6 +80,42 @@ public class ProductController {
         response = productService.deleteById(productId);
         if (response.isError()) {
             return new ResponseEntity<>(response, HttpStatus.BAD_REQUEST);
+        }
+
+        return new ResponseEntity<>(response, HttpStatus.OK);
+    }
+
+    @PostMapping("/products/add-product")
+    public ResponseEntity<ProductResponse> addProduct(@RequestParam("name") String name,
+                                                      @RequestParam("description") String description,
+                                                      @RequestParam("price") Double price,
+                                                      @RequestParam("images") MultipartFile multipart) {
+
+        Product product = new Product();
+        product.setName(name);
+        product.setDescription(description);
+        product.setPrice(price);
+        ProductResponse response = productService.save(product);
+        if (response.isError()) {
+            return new ResponseEntity<>(response, HttpStatus.BAD_REQUEST);
+        }
+
+        Product theProduct = response.getProduct();
+
+        try {
+            String fileName = multipart.getOriginalFilename();
+            assert fileName != null;
+            String newFileName = S3Util.urlFolderProduct + theProduct.getId() + fileName.substring(fileName.lastIndexOf('.'));
+            S3Util.uploadFile(newFileName, multipart.getInputStream());
+            String urlImage = "https://" + S3Util.bucketName + ".s3.amazonaws.com/" + newFileName;
+
+            theProduct.setImages(urlImage);
+            response = productService.update(theProduct.getId(), theProduct);
+            response.setMessage("Added product id - " + theProduct.getId());
+        } catch (IOException ex) {
+            response.setError(true);
+            response.setMessage("Error uploading file: " + ex.getMessage());
+            return new ResponseEntity<>(response, HttpStatus.INTERNAL_SERVER_ERROR);
         }
 
         return new ResponseEntity<>(response, HttpStatus.OK);
